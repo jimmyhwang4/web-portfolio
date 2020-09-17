38831a166846a244ccf2b89a1ac79f9bd1cb2ceb
jimmy <jh5397@nyu.edu>
2020-09-17 05:21:20 -0700

    Projects update

--- src/pages/ProjectsWebCovid19.js
+++ src/pages/ProjectsWebCovid19.js
@@ -5,6 +5,8 @@ import HeaderMobile from '../components/HeaderMobile';
 import ProjectsMenu from '../components/ProjectsMenu';
 import Footer from '../components/Footer';
 
+import ModalImage from "react-modal-image";
+
 function ProjectsWebCovid19() {
     return (
         <div>
@@ -22,15 +24,55 @@ function ProjectsWebCovid19() {
                         <video autoPlay controls muted>
                             <source src="/projects/web-dev/covid-19/covid-19.mp4" controls type="video/mp4" />
                         </video>
+                        <h2>Web View</h2>
                         <div className="projectPageWeb">
-                            <h2>Web View</h2>
-                            <div className="projectPageWebImg">
-                                <img src="/projects/web-dev/covid-19/covid-19-1.png"></img>
-                                <img src="/projects/web-dev/covid-19/covid-19-2.png"></img>
-                                <img src="/projects/web-dev/covid-19/covid-19-3.png"></img>
-                                <img src="/projects/web-dev/covid-19/covid-19-4.png"></img>
-                                <img src="/projects/web-dev/covid-19/covid-19-5.png"></img>
-                                <img src="/projects/web-dev/covid-19/covid-19-6.png"></img>
+                            <div id="projectPageWebImg">
+                                <ModalImage
+                                    small="/projects/web-dev/covid-19/covid-19-1.png"
+                                    large="/projects/web-dev/covid-19/covid-19-1.png"
+                                    hideDownload="true"
+                                    hideZoom="true"
+                                />
+                            </div>
+                            <div id="projectPageWebImg">
+                                <ModalImage
+                                    small="/projects/web-dev/covid-19/covid-19-2.png"
+                                    large="/projects/web-dev/covid-19/covid-19-2.png"
+                                    hideDownload="true"
+                                    hideZoom="true"
+                                />
+                            </div>
+                            <div id="projectPageWebImg">
+                                <ModalImage
+                                    small="/projects/web-dev/covid-19/covid-19-3.png"
+                                    large="/projects/web-dev/covid-19/covid-19-3.png"
+                                    hideDownload="true"
+                                    hideZoom="true"
+                                />
+                            </div>
+                            <div id="projectPageWebImg">
+                                <ModalImage
+                                    small="/projects/web-dev/covid-19/covid-19-4.png"
+                                    large="/projects/web-dev/covid-19/covid-19-4.png"
+                                    hideDownload="true"
+                                    hideZoom="true"
+                                />
+                            </div>
+                            <div id="projectPageWebImg">
+                                <ModalImage
+                                    small="/projects/web-dev/covid-19/covid-19-5.png"
+                                    large="/projects/web-dev/covid-19/covid-19-5.png"
+                                    hideDownload="true"
+                                    hideZoom="true"
+                                />
+                            </div>
+                            <div id="projectPageWebImg">
+                                <ModalImage
+                                    small="/projects/web-dev/covid-19/covid-19-6.png"
+                                    large="/projects/web-dev/covid-19/covid-19-6.png"
+                                    hideDownload="true"
+                                    hideZoom="true"
+                                />
                             </div>
                         </div>
                     </div>
--- src/pages/ProjectsWebImager.js
+++ src/pages/ProjectsWebImager.js
@@ -5,6 +5,8 @@ import HeaderMobile from '../components/HeaderMobile';
 import ProjectsMenu from '../components/ProjectsMenu';
 import Footer from '../components/Footer';
 
+import ModalImage from "react-modal-image";
+
 function ProjectsWebImager() {
     return (
         <div>
@@ -22,20 +24,58 @@ function ProjectsWebImager() {
                         <video autoPlay controls muted>
                             <source src="/projects/web-dev/imager/imager.mp4" controls type="video/mp4" />
                         </video>
+                        <h2>Web View</h2>
                         <div className="projectPageWeb">
-                            <h2>Web View</h2>
-                            <div className="projectPageWebImg">
-                                <img src="/projects/web-dev/imager/imager-1.png"></img>
-                                <img src="/projects/web-dev/imager/imager-2.png"></img>
-                                <img src="/projects/web-dev/imager/imager-3.png"></img>
+                            <div id="projectPageWebImg">
+                                <ModalImage
+                                    small="/projects/web-dev/imager/imager-1.png"
+                                    large="/projects/web-dev/imager/imager-1.png"
+                                    hideDownload="true"
+                                    hideZoom="true"
+                                />
+                            </div>
+                            <div id="projectPageWebImg">
+                                <ModalImage
+                                    small="/projects/web-dev/imager/imager-2.png"
+                                    large="/projects/web-dev/imager/imager-2.png"
+                                    hideDownload="true"
+                                    hideZoom="true"
+                                />
+                            </div>
+                            <div id="projectPageWebImg">
+                                <ModalImage
+                                    small="/projects/web-dev/imager/imager-3.png"
+                                    large="/projects/web-dev/imager/imager-3.png"
+                                    hideDownload="true"
+                                    hideZoom="true"
+                                />
                             </div>
                         </div>
+                        <h2>Mobile View</h2>
                         <div className="projectPageMobile">
-                            <h2>Mobile View</h2>
-                            <div className="projectPageMobileImg">
-                                <img src="/projects/web-dev/imager/imager-mobile-1.png"></img>
-                                <img src="/projects/web-dev/imager/imager-mobile-2.png"></img>
-                                <img src="/projects/web-dev/imager/imager-mobile-3.png"></img>
+                            <div id="projectPageMobileImg">
+                                <ModalImage
+                                    small="/projects/web-dev/imager/imager-mobile-1.png"
+                                    large="/projects/web-dev/imager/imager-mobile-1.png"
+                                    hideDownload="true"
+                                    hideZoom="true"
+                                />
+                            </div>
+                            <div id="projectPageMobileImg">
+                                <ModalImage
+                                    small="/projects/web-dev/imager/imager-mobile-2.png"
+                                    large="/projects/web-dev/imager/imager-mobile-2.png"
+                                    hideDownload="true"
+                                    hideZoom="true"
+                                />
+                            </div>
+                            <div id="projectPageMobileImg">
+                                <ModalImage
+                                    small="/projects/web-dev/imager/imager-mobile-3.png"
+                                    large="/projects/web-dev/imager/imager-mobile-3.png"
+                                    hideDownload="true"
+                                    hideZoom="true"
+                                />
                             </div>
                         </div>
                     </div>
